6e2a0c13a6b7c6f4cf5e29fef094ee048869abe3
doardanunes <60709759+doardanunes@users.noreply.github.com>
2020-07-27 22:54:55 -0300

    hello

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public form = [
+    { val: 'Manter-se conectado', isChecked: true }
+  ];
+
+  public mensagem;
+  public email;
+  public senha;
+
   constructor() {}
 
+  public registrar(){
+    if(this.email && this.senha == null) {
+    this.mensagem = "Login não realizado!";
+     }
+    else {
+    this.mensagem = "Login Realizado!";
+    }}
+    
+    public apagar(){
+      this.email = null;
+      this.senha = null;
+      this.mensagem = null;
+    }
 }
